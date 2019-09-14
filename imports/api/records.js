7719776994829ba909fc38d89ad539c3857770dd
Nero Cui <nerocui@outlook.com>
2019-09-14 15:00:57 -0700

    added algolia on each record entry

--- imports/api/records.js
+++ imports/api/records.js
@@ -2,16 +2,22 @@ import {Meteor} from 'meteor/meteor';
 import {Records} from './db';
 import API from '../constants/methods';
 import { isAuthenticated } from '../util/authUtil';
+import algoliasearch from 'algoliasearch';
 
-const useRecordsAPI = (recordTemplate) => {
-	Meteor.methods({
-		[API.RECORD_API.INSERT](record) {
-			if (!isAuthenticated()) {
-				throw new Meteor.Error("Not authenticated");
-			}
-			Records.insert({record});
-		},
-	});
-};
+const key = Meteor.settings.apis.filter(api => api.id === "ALGOLIA")[0].value;
+const client = algoliasearch(key.algoliaApplicationID, key.algoliaAdminKey);
+const userIndex = client.initIndex('gaby_davis_records');
 
-export default useRecordsAPI;
+Meteor.methods({
+	[API.RECORD_API.INSERT](record) {
+		if (!isAuthenticated()) {
+			throw new Meteor.Error("Not authenticated");
+		}
+		const _id = Records.insert({record});
+		userIndex.addObject({
+			objectID: _id,
+			...record
+		});
+	},
+});
+
