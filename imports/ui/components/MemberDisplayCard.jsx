c4aaf038b4d7d785b1c993461ec77269d4ff756d
Barry Ki <barry@ensemble.com>
2019-09-15 10:31:38 -0700

    delete person

--- imports/ui/components/MemberDisplayCard.jsx
+++ imports/ui/components/MemberDisplayCard.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Meteor } from "meteor/meteor";
 import { withTracker } from "meteor/react-meteor-data";
 
+import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
+import ClearIcon from '@material-ui/icons/Clear';
+import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -20,13 +22,24 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const MemberDisplayCard = ({member, columns}) => {
+const MemberDisplayCard = ({member, columns, onDelete}) => {
   const classes = useStyles();
+
+  const deleteMember = () => {
+    onDelete(member)
+  }
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography variant="h6">{member.role}</Typography>
+        <Box display="flex">
+          <Box display="flex" flexGrow={1}>
+            <Typography variant="h6">{member.role}</Typography>
+          </Box>
+          <IconButton size="small" onClick={deleteMember}>
+            <ClearIcon />
+          </IconButton>
+        </Box>
         
         {columns.map(column => {
           if (column.type === "string" && column.field !== "role") {
