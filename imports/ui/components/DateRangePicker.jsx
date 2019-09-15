e8c307f8d606ccf8d916bcd73494134c5fed08a8
Breyanne Bautista <breyanneb@ensemble.com>
2019-09-14 17:32:36 -0700

    Npm package used

--- imports/ui/components/DateRangePicker.jsx
+++ imports/ui/components/DateRangePicker.jsx
@@ -1,56 +1,35 @@
 import * as React from "react";
-import { DateRangePicker } from "@bit/atto-byte.components.date-range";
-import { createMuiTheme } from "@material-ui/core/styles";
-import { ThemeProvider, styled } from "@material-ui/styles";
-const theme = createMuiTheme();
+import { styled } from "@material-ui/styles";
+
+/*  1. Import material date range picker */
+import { DateRangePicker } from 'material-date-range-picker';
 
 const Wrapper = styled("div")({
   width: "350px",
   padding: "10px"
 });
 
-/**
- * Props must include:
- *  - startDate (string)
- *  - endDate (Date)
- *  - onDateRangeSubmit (function)
- */
-
-/** Example usage:
-        <DateRangePicker
-          startDate={'2019-09-04'}
-          endDate={'2019-09-14'}
-          onDateRangeSubmit={...}
-        />
- */
-
-class MyDateRangePicker extends React.Component {
+class App extends React.Component {
   state = {
-    fromDate: this.props.startDate,
-    toDate: this.props.endDate
+    fromDate: null,
+    toDate: null
   };
-
-  _handleDateRangeChange = update => {
-    this.setState(update)
-    // TODO: uncomment once onDateRangeSubmit implemented
-    // this.props.onDateRangeSubmit(update);
-  };
+  /*  2. Add state handler */
+  _handleDateRangeChange = update => this.setState(update);
 
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <Wrapper>
-          <DateRangePicker
-            fromDate={this.state.fromDate}
-            toDate={this.state.toDate}
-            onChange={this._handleDateRangeChange}
-            closeDialogOnSelection={false}
-            dateFormat={"YYYY-MM-DD"}
-          />
-        </Wrapper>
-      </ThemeProvider>
+      <Wrapper>
+        {/* 3. Add the material date range picker in your project */}
+        <DateRangePicker
+          fromDate={this.state.fromDate}
+          toDate={this.state.toDate}
+          onChange={this._handleDateRangeChange}
+          closeDialogOnSelection
+        />
+      </Wrapper>
     );
   }
 }
 
-export default MyDateRangePicker;
+export default App;
